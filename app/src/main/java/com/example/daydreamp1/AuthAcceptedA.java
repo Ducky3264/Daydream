8a8a8d9eebdfb764e0366d5667009767c1044ce6
Ducky3264 <49353182+Ducky3264@users.noreply.github.com>
2021-08-01 23:36:48 -0500

    forgot to add these files in previously...

--- app/src/main/java/com/example/daydreamp1/AuthAcceptedA.java
+++ app/src/main/java/com/example/daydreamp1/AuthAcceptedA.java
@@ -6,7 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import net.openid.appauth.AuthState
+
 import net.openid.appauth.AuthState;
 import net.openid.appauth.AuthorizationException;
 import net.openid.appauth.AuthorizationResponse;
